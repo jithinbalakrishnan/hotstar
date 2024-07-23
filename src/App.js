5ef3f0e30c4da7cea3518cf5c9e9b0c5bdf0060a
jithinbalu <jithinbalakrishnan00@gmail.com>
2024-07-23 14:33:56 +0530

    React set-up

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const App = () => (
+    <>
+        <p>Hotstar</p>
+    </>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
